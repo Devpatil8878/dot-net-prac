81c24932bfd9070c3cca6c5d0ae7ce3e84111362
devpatil8878 <devanandpatil8878@gmail.com>
2024-10-16 15:55:11 +0530

    sdfs

---
+++ WebApplication1/Controllers/TasksController.cs
@@ -0,0 +1,76 @@
+﻿
+using WebApplication1.Data;
+using WebApplication1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class TasksController : Controller
+    {
+
+        private readonly TaskRepository _taskRepository;
+
+        public TasksController()
+        {
+            _taskRepository = new TaskRepository();
+        }
+
+        public IActionResult Index()
+        {
+            var tasks = _taskRepository.GetAllTasks();
+            return View(tasks);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(TaskItem task)
+        {
+            _taskRepository.AddTask(task);
+            return RedirectToAction("index");
+            
+            return View(task);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var task = _taskRepository.GetTaskById(id);
+            if(task == null)
+            {
+                return NotFound();
+            }
+            return View(task);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(TaskItem task)
+        {
+
+            _taskRepository.UpdateTask(task);
+            return RedirectToAction("Index");
+            
+
+            return View(task);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var task = _taskRepository.GetTaskById(id);
+            if (task == null)
+                return NotFound();
+
+            return View(task);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            _taskRepository.DeleteTask(id);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ WebApplication1/Data/TaskRepository.cs
@@ -0,0 +1,53 @@
+﻿using WebApplication1.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebApplication1.Data
+{
+    public class TaskRepository
+    {
+        private List<TaskItem> _tasks = new List<TaskItem>();
+        private int _nextId = 1;
+
+
+
+        public List<TaskItem> GetAllTasks()
+        {
+            return _tasks;
+        }
+        
+
+        public TaskRepository()
+        {
+            _tasks.Add(new TaskItem { Id = _nextId++, Title = "Task 1", IsCompleted = false });
+            _tasks.Add(new TaskItem { Id = _nextId++, Title = "Task 2", IsCompleted = true });
+        }
+
+
+
+        public void AddTask(TaskItem task)
+        {
+            task.Id = _nextId++;
+            _tasks.Add(task);
+            foreach(var item in _tasks)
+                Console.WriteLine(item.Title);
+        }
+
+        public TaskItem GetTaskById(int id) => _tasks.FirstOrDefault(t => t.Id == id);
+
+        public void UpdateTask (TaskItem UpdatedTask)
+        {
+            var task = GetTaskById(UpdatedTask.Id);
+            if (task != null) { 
+                task.Title = UpdatedTask.Title;
+                task.IsCompleted = UpdatedTask.IsCompleted;
+            }
+        }
+
+        public void DeleteTask(int id)
+        {
+            var task = GetTaskById(id);
+            if (task != null) _tasks.Remove(task);
+        }
+    }
+}
